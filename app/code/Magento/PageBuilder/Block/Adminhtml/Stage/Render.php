be4437901990eb6c36d5713c713d993387a05a36
Saurabh Kumar <glo17680@adobe.com>
2024-12-23 21:12:41 +0530

    AC-13306::Adobe Commerce 2.4.8 core code is compatible with PHP 8.4

--- app/code/Magento/PageBuilder/Block/Adminhtml/Stage/Render.php
+++ app/code/Magento/PageBuilder/Block/Adminhtml/Stage/Render.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Copyright © Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
+ * Copyright 2019 Adobe
+ * All Rights Reserved.
  */
 
 declare(strict_types=1);
@@ -15,11 +15,6 @@ use Magento\RequireJs\Model\FileManager;
 use Magento\PageBuilder\Model\Stage\Config;
 use Magento\Framework\Serialize\Serializer\Json;
 
-/**
- * Class Render
- *
- * @api
- */
 class Render extends Template
 {
     /**
